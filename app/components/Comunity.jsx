9402ad4cca6919bdd534895ba78e7df271f5de99
aidyn <aidyn.meiramkhanuly@gmail.com>
2024-04-11 23:59:57 +0500

    comunity

--- app/components/Comunity.jsx
+++ app/components/Comunity.jsx
@@ -6,10 +6,10 @@ const Comunity = () => {
   return (
     <section className="container mx-auto mb-40">
       <div className="flex flex-col items-center text-center w-1/2 mx-auto mb-10 gap-5">
-        <h2>Built by the Community for the Community</h2>
-        <p>Harum quae dolore orrupti aut temporibus ariatur.</p>
+        <p className=" text-green">Команда</p>
+        <h2>Вдохновленная Команда, Готовая Привести Вас к Успеху!</h2>
       </div>
-      <div className="flex flex-wrap justify-center w-1/2 mx-auto gap-2">
+      <div className="flex flex-wrap justify-center w-full mx-auto gap-2">
         {comunityList.map((item, index) => (
           <Image
             width={300}
