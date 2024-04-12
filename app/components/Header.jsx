99ff1d96ce33b32bc5e7158e34ffeb339e37782c
aidyn <aidyn.meiramkhanuly@gmail.com>
2024-04-12 12:47:23 +0500

    add header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="pb-20">
-      <nav className="fixed overflow-hidden z-20 w-full bg-white/80 dark:bg-gray-950/0 dark:shadow-md rounded-b-lg dark:shadow-gray-950/10 border-b border-[--ui-light-border-color] border-x dark:border-[--ui-dark-border-color] backdrop-blur-2xl">
+      <nav className="fixed z-20 w-full bg-black/80 backdrop-blur-sm">
         <div className="px-6 m-auto max-w-6xl 2xl:px-0">
           <div className="flex flex-wrap items-center justify-between py-2 sm:py-4">
             <div className="w-full items-center flex justify-between lg:w-auto">
@@ -16,58 +16,28 @@ const Header = () => {
                   alt="craftcode logo"
                 />
               </a>
-              <div className="flex lg:hidden">
-                <button
-                  aria-label="humburger"
-                  id="menu"
-                  className="relative border bordeer-gray-950/30 dark:border-white/20 size-9 rounded-full transition duration-300 active:scale-95"
-                >
-                  <div
-                    aria-hidden="true"
-                    id="line1"
-                    className="m-auto h-[1.5px] w-4 rounded bg-gray-900 transition duration-300 dark:bg-gray-300"
-                  ></div>
-                  <div
-                    aria-hidden="true"
-                    id="line2"
-                    className="m-auto mt-1.5 h-[1.5px] w-4 rounded bg-gray-900 transition duration-300 dark:bg-gray-300"
-                  ></div>
-                </button>
-              </div>
             </div>
             <div className="w-full h-0 lg:w-fit flex-wrap justify-end items-center space-y-8 lg:space-y-0 lg:flex lg:h-fit md:flex-nowrap">
-              <div className="mt-6 text-gray-600 dark:text-gray-300 md:-ml-4 lg:pr-4 lg:mt-0">
-                <ul className="space-y-6 tracking-wide text-base lg:text-sm lg:flex lg:space-y-0">
+              <div className="mt-6 text-gray-600 md:-ml-4 lg:pr-4 lg:mt-0">
+                <ul className="flex text-white lg:text-sm lg:flex lg:space-y-0">
                   <li>
-                    <a
-                      href="#"
-                      className="block md:px-4 transition hover:text-primary-600 dark:hover:text-primary-400"
-                    >
-                      <span>Product</span>
+                    <a href="#" className="md:px-4 duration-75 hover:border-b">
+                      Product
                     </a>
                   </li>
                   <li>
-                    <a
-                      href="#"
-                      className="block md:px-4 transition hover:text-primary-600 dark:hover:text-primary-400"
-                    >
-                      <span>Playground</span>
+                    <a href="#" className=" md:px-4 hover:border-b">
+                      Playground
                     </a>
                   </li>
                   <li>
-                    <a
-                      href="#"
-                      className="block md:px-4 transition hover:text-primary-600 dark:hover:text-primary-400"
-                    >
-                      <span>Docs</span>
+                    <a href="#" className="md:px-4 hover:border-b">
+                      Docs
                     </a>
                   </li>
                   <li>
-                    <a
-                      href="#"
-                      className="block md:px-4 transition hover:text-primary-600 dark:hover:text-primary-400"
-                    >
-                      <span>Blog</span>
+                    <a href="#" className="md:px-4 hover:border-b">
+                      Blog
                     </a>
                   </li>
                 </ul>
