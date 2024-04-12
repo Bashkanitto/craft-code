e24c967aaaa63d1c3e888f1f249e13be31ab1c7e
aidyn <aidyn.meiramkhanuly@gmail.com>
2024-04-12 12:48:27 +0500

    fix hero

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,17 +1,13 @@
-import { heroList } from "./Constants";
-
 const Hero = () => {
   return (
-    <section className="relative flex flex-col  justify-center items-center text-white text-center gap-10">
+    <section className="container flex flex-col justify-center items-center text-center gap-10 mb-40">
       <button className="p-1 pr-2 border border-gray-700 rounded-full flex gap-2 items-center">
         <span className="bg-green p-1 px-2 rounded-full">New</span>Introducing
         to development
       </button>
-      <h1 className="text-6xl font-bold w-2/3">
-        Кодируй Свои Мечты в Реальность c Craftcode
-      </h1>
+      <h1 className="text-6xl font-bold">Crafting Code, Crafting Careers!</h1>
 
-      <p className="w-1/2 text-xl">
+      <p className="w-2/3 text-xl">
         Раскрой свой потенциал в программировании: продвинутые концепции в нашей
         офлайн-школе программирования
       </p>
@@ -19,17 +15,6 @@ const Hero = () => {
         <button className=" p-3 px-6 bg-green rounded-xl text-xl">
           Связатся
         </button>
-      </div>
-      <div className="mb-40 cards w-[500px] h-60 overflow-hidden border border-gray-700 rounded-2xl flex justify-center items-cente">
-        {heroList.map((item) => (
-          <div
-            className="flex flex-col gap-5 justify-center min-w-[400px]"
-            key={item.id}
-          >
-            <h2 className="text-4xl">{item.h2}</h2>
-            <p className="text-xl">{item.p}</p>
-          </div>
-        ))}
       </div>
     </section>
   );
