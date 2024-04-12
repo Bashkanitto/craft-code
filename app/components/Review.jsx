85d41dc68b531be0f760d0827d298dba78af6aa9
aidyn <aidyn.meiramkhanuly@gmail.com>
2024-04-12 13:47:42 +0500

    review ready

--- app/components/Review.jsx
+++ app/components/Review.jsx
@@ -3,29 +3,25 @@ import { reviewList } from "./Constants";
 
 const Review = () => {
   return (
-    <section className="container mx-auto flex flex-col  justify-center items-center text-white text-center gap-5 mb-40">
-      <p className=" text-green">Отзывы</p>
-      <h2 className="mb-5">Каждый отзыв - это история успеха</h2>
-      <div className="blocks flex flex-col md:flex-row flex-wrap gap-2">
-        {reviewList.map((item) => (
-          <div
-            className="border border-gray-700 flex-wrap p-10 rounded-2xl flex flex-col justify-center items-center"
-            key={item.id}
-          >
-            <div className="flex gap-4 items-center">
-              <Image
-                width={20}
-                height={20}
-                src={item.img}
-                alt={item.h2}
-                className="rounded-full w-full"
-              />
-              <h2>{item.h2}</h2>
-            </div>
-            <p>{item.p}</p>
+    <section className="container mx-auto flex flex-col lg:flex-row justify-center items-center gap-5 mb-40">
+      {reviewList.map((item) => (
+        <div
+          key={item.id}
+          className="flex flex-col gap-5 border p-5 rounded-2xl h-full sm:h-60"
+        >
+          <div className="flex items-center gap-5">
+            <Image
+              width={50}
+              height={50}
+              src={item.img}
+              alt={item.h2}
+              className="rounded-full w-10 h-10 object-cover"
+            />
+            <span>{item.h2}</span>
           </div>
-        ))}
-      </div>
+          <p className="text-xs">{item.p}</p>
+        </div>
+      ))}
     </section>
   );
 };
