43e3f43287ac31188f5ede08f42127015c44dd14
aidyn <aidyn.meiramkhanuly@gmail.com>
2024-04-12 12:47:47 +0500

    fixups

--- app/components/CustomButton.jsx
+++ app/components/CustomButton.jsx
@@ -1,6 +1,6 @@
 export const CustomButton = ({ children }) => {
   return (
-    <button className="btn bg-violet-500 text-white text-xs rounded-3xl w-40 h-10 ml-1 flex items-center hover:bg-transparent border border-violet-500 duration-100 justify-center">
+    <button className="btn bg-green text-white text-xs rounded-3xl w-40 h-10 ml-1 flex items-center hover:bg-transparent hover:border hover:border-green duration-100 justify-center">
       {children}
     </button>
   );
--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
+import About from "./components/About";
 import Advantages from "./components/Advantages";
 import Comunity from "./components/Comunity";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Stack from "./components/Stack";
+import Info from "./components/Info";
 import Review from "./components/review";
 
 export default function Home() {
@@ -13,10 +14,11 @@ export default function Home() {
       <Header />
       <main className="overflow-hidden text-white">
         <Hero />
+        <About />
         <Advantages />
-        <Stack />
         <Comunity />
         <Review />
+        <Info />
         <Contact />
       </main>
       <Footer />
